6ed6533d09c439bf911df35b9ea3e1f77ad810a5
Aditya5489 <ap5663019@gmail.com>
2026-02-28 12:37:32 +0530

    Updating frontend URL

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://agentic-ai-frontend-silk.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
